907a1398c83a4d132dd4c28bf119f1e440bbeaf6
Devon Poston <dev.poston@gmail.com>
2021-07-03 11:01:14 -0700

    first naive implementation of MySQL seeding script

--- mySqlSeeder.js
+++ mySqlSeeder.js
@@ -15,3 +15,48 @@ connection.connect((err) => {
   }
   console.log('CONNECTED TO MYSQL!');
 });
+
+const seedScript = (id = 1, count = 1, tagCycle = 1) => {
+  let productId = id;
+  let cycle = count;
+  let imgUrl = 'https://picsum.photos/200';
+  let rndNum = Math.floor(Math.random() * (6 - 1) + 1);
+  let rndTag = ['tech', 'animals', 'arch', 'people', 'nature'];
+  let options = [productId, imgUrl, rndNum];
+
+  if (tagCycle === 1) {
+    for (let i = 0; i < 5; i++) {
+      connection.query('INSERT INTO tags (tag_name) VALUES (?)', [rndTag[i]], (err, data) => {
+        if (err) {
+          console.log('MYSQL Error:', err);
+        } else {
+          console.log('MYSQL SUCCESS:', data);
+        }
+      });
+    }
+  }
+
+  if (productId <= 10000000) {
+    connection.query('INSERT INTO images (product_id, img_url, tag_id) VALUES (?, ?, ?) ', options, (err, data) => {
+      if (err) {
+        console.log('MYSQL Error:', err);
+      } else {
+        console.log('MYSQL SUCCESS:', productId, data);
+        if (cycle === 5) {
+          seedScript(productId += 1, 1, null);
+        } else {
+          seedScript(productId, cycle += 1, null);
+        }
+      }
+    });
+  }
+
+};
+seedScript();
+
+// const { Sequelize } = require('sequelize');
+// const databaseName = 'images';
+// const sequelize = new Sequelize('productImages', 'root', '', {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
--- schema.sql
+++ schema.sql
@@ -2,11 +2,15 @@ CREATE DATABASE productgallery;
 
 USE productgallery;
 
-CREATE TABLE product (
-  id int not null primary key AUTO_INCREMENT,
-  product_id varchar(255)
+CREATE TABLE images (
+  product_id int,
+  img_url varchar(255),
+  tag_id int
 );
 
-CREATE TABLE images (
-  img_url varchar(255)
+CREATE TABLE tags (
+  id int not null primary key AUTO_INCREMENT,
+  tag_name varchar(255)
 );
+
+ALTER TABLE images ADD FOREIGN KEY (tag_id) REFERENCES tags (id);
